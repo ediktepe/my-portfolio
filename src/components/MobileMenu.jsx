dc0bdb48581b0e68ed41d423d97235663bc29416
ediktepe <esaddiktepe@gmail.com>
2025-03-20 01:45:26 +0100

    navbar close button position fix

--- src/components/MobileMenu.jsx
+++ src/components/MobileMenu.jsx
@@ -11,7 +11,7 @@ export const MobileMenu = ({menuOpen, setMenuOpen}) => {
             }
             `}>
             <button onClick={() => setMenuOpen(false)} 
-                    className="absolute top-6 right-6 text-white text-3xl focus:outline-none cursor-pointer"
+                    className="absolute top-4 right-7 text-white text-3xl focus:outline-none cursor-pointer"
                     aria-label="Close Menu">
                 &times;
             </button>
